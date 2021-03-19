d77fb82dc697998afc0a76f74755518350a0cb35
Sundar Venkataraman <sundarvenkata@ebi.ac.uk>
2021-03-19 09:13:31 +0000

    Remove two mongo instances in replica set to avoid races during tests

--- scripts/create_mongo_single_node_sharded_env.sh
+++ scripts/create_mongo_single_node_sharded_env.sh
@@ -24,13 +24,11 @@ rm -rf /data/mongodb
 mkdir -p /data/mongodb/shard01 /data/mongodb/shard02 /data/mongodb/config
 mongod --shardsvr --port 27018 --replSet rs0 --dbpath /data/mongodb/shard01 &
 wait_for_mongo "localhost:27018"
-mongod --shardsvr --port 27020 --replSet rs0 --dbpath /data/mongodb/shard02 &
-wait_for_mongo "localhost:27020"
-mongo --port 27018 --eval 'rs.initiate({_id:"rs0", members: [{_id: 1, host: "localhost:27018"}, {_id: 2, host: "localhost:27020"}]})'
+mongo --port 27018 --eval 'rs.initiate({_id:"rs0", members: [{_id: 1, host: "localhost:27018"}]})'
 
 mongod --configsvr --port 27019 --replSet rs1 --dbpath /data/mongodb/config &
 wait_for_mongo "localhost:27019"
 mongo --port 27019 --eval 'rs.initiate()'
 mongos --configdb rs1/localhost:27019 --port 27017 &
 wait_for_mongo "localhost:27017"
-mongo --eval 'sh.addShard("rs0/localhost:27018,localhost:27020")'
+mongo --eval 'sh.addShard("rs0/localhost:27018")'
