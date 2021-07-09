77cdd8b7dd5350de2520809de125b671347867ff
April Shen <ashen@ebi.ac.uk>
2021-07-09 11:26:49 +0100

    bump version (to get a unique name on pypi)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='ebi_eva_common_pyutils',
     packages=find_packages(),
-    version='0.3.14',
+    version='0.3.15',
     license='Apache',
     description='EBI EVA - Common Python Utilities',
     url='https://github.com/EBIVariation/eva-common-pyutils',
