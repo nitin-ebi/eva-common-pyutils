0e6a1d2647511059ea603532cc0b93a85e549dc4
Timothee Cezard <tcezard@ebi.ac.uk>
2021-09-27 11:44:35 +0100

    Address review comments

--- ebi_eva_common_pyutils/ncbi_utils.py
+++ ebi_eva_common_pyutils/ncbi_utils.py
@@ -1,4 +1,5 @@
 import requests
+from retry import retry
 
 from ebi_eva_common_pyutils.logger import logging_config as log_cfg
 
@@ -12,14 +13,19 @@ efetch_url = eutils_url + 'efetch.fcgi'
 ensembl_url = 'http://rest.ensembl.org/info/assembly'
 
 
+@retry(tries=3, delay=2, backoff=1.2, jitter=(1, 3))
 def get_ncbi_assembly_dicts_from_term(term):
     payload = {'db': 'Assembly', 'term': '"{}"'.format(term), 'retmode': 'JSON'}
-    data = requests.get(esearch_url, params=payload).json()
+    req = requests.get(esearch_url, params=payload)
+    req.raise_for_status()
+    data = req.json()
     assembly_dicts = []
     if data:
         assembly_id_list = data.get('esearchresult').get('idlist')
         payload = {'db': 'Assembly', 'id': ','.join(assembly_id_list), 'retmode': 'JSON'}
-        summary_list = requests.get(esummary_url, params=payload).json()
+        req = requests.get(esummary_url, params=payload).json()
+        req.raise_for_status()
+        summary_list = req.json()
         for assembly_id in summary_list.get('result', {}).get('uids', []):
             assembly_dicts.append(summary_list.get('result').get(assembly_id))
     return assembly_dicts
