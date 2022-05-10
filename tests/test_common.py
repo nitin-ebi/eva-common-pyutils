46257dbe762a9cc53ffbbf7dbdb3430259ca8151
Timothee Cezard <tcezard@ebi.ac.uk>
2022-05-10 14:47:50 +0100

    add tests for common utils function

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,8 +1,20 @@
 import os
 from unittest import TestCase
 
+from more_itertools import grouper
+
+from ebi_eva_common_pyutils.common_utils import merge_two_dicts, grouper
+
 
 class TestCommon(TestCase):
 
     resources_folder = os.path.join(os.path.dirname(__file__), 'resources')
 
+
+class TestCommonUtils(TestCase):
+
+    def test_merge_two_dicts(self):
+        d1 = {'a': 1, 'b': 2, 'c': 3}
+        d2 = {'d': 4, 'a': 5, 'e': 6}
+        assert merge_two_dicts(d1, d2) == {'a': 5, 'b': 2, 'c': 3, 'd': 4, 'e': 6}
+        assert merge_two_dicts(d2, d1) == {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 6}
